c01e11d2ad1bce27d4260c5f405dab56fba4a31c
Vitor Mateus <oliveirasantos.vm@gmail.com>
2020-09-27 14:53:22 -0300

    Add .gitignore, instantiated express, path, bodyparser, install ejs, create folders models, views and controllers (MVC), prepareted researchRoute, and create homepage for research.

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const bodyparser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const researchModule = require('./routes/researchRoute');
+app.use(researchModule);
+
+app.listen(3000, () => { console.log("Listening on Port 3000...") });
